6aae187f8874b6122f62ebae3c6a4b2b4f211ad9
Miguel Angel Lemos Padilla <11946083+Uroboros94@users.noreply.github.com>
2026-04-10 10:27:46 -0500

    Refactor PrismaClient initialization in db.ts

--- cardmkt-backend/src/lib/db.ts
+++ cardmkt-backend/src/lib/db.ts
@@ -1,7 +1,5 @@
 import { PrismaClient } from '@prisma/client'
- 
-const prisma = new PrismaClient({
+
+export const db = new PrismaClient({
   log: process.env.NODE_ENV !== 'production' ? ['query', 'warn', 'error'] : ['error'],
 })
- 
-export const db prisma
